0b44bf5f3c79abc2d1306e39e78ef1b5bb8351c8
Stathis Fotiadis <sfotiadis@gmail.com>
2015-11-14 16:15:17 +0100

    Addi resize and mouse callbacks

--- src/kandykrash.cpp
+++ src/kandykrash.cpp
@@ -14,13 +14,15 @@
 const int ROWS = 12;
 const int COLUMNS = 15;
 const int TILESIZE = 40;
-const int PADDING = 2;
+const int PADDING = 0;
 const int STATUSLINE = 20;
 
 // Width: 40 X 15 columns = 600 + padding (16*2) = 632
 GLsizei winWidth = COLUMNS * TILESIZE + (COLUMNS + 1) * PADDING;
 // Height: 40 X 12 rows = 480 + padding (13*2) + status line (20) = 526
 GLsizei winHeight = ROWS * TILESIZE + (ROWS + 1) * PADDING + STATUSLINE;
+
+int grid[ROWS][COLUMNS];
 
 void initRendering() {
 	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
@@ -30,14 +32,35 @@ void display() {
    glFlush();
 }
 
+/*
+glutMouseFunc(void (*func)(int button, int state, int x, int y))
+mouse function callback
+button: GLUT_LEFT_BUTTON, GLUT_MIDDLE_BUTTON, GLUT_RIGHT_BUTTON
+state: GLUT_UP, GLUT_DOWN
+x, y: mouse coordinates
+*/
+void checkGrid(GLint button, GLint state, GLint x, GLint y) {
+	//
+}
+
+void resize(int width, int height) {
+    // we ignore the params and do:
+    glutReshapeWindow(winWidth, winHeight);
+}
+
 int main(int argc,char** argv) {
 	glutInit(&argc, argv);
-	// glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB);
+	glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB);
 	glutInitWindowPosition (50, 50);
 	glutInitWindowSize (winWidth, winHeight);
 	glutCreateWindow ("Kandy Krash");
+
 	initRendering();
+
    	glutDisplayFunc(display);
+   	glutReshapeFunc(resize);
+	glutMouseFunc(checkGrid);
+
 	glutMainLoop();
 	return 0;
 }
